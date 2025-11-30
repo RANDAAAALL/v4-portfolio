7b6fd3dee5b326586cc89386b9f329cf54a279be
RANDAAAALL <lesterandig17@gmail.com>
2025-11-30 15:03:15 +0800

    Refactor(layout.tsx): change defaultTheme to white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="white" enableSystem disableTransitionOnChange>
           <Suspense fallback={null}>{children}</Suspense>
           <ToasterClient />
           <Analytics />
