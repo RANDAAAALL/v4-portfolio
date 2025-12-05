db17594894f67b0b317408e056bd14b2777135f0
RANDAAAALL <lesterandig17@gmail.com>
2025-12-05 21:59:39 +0800

    Feat: replace old with new cv and added new techstack on projects array

--- src/lib/values/project.ts
+++ src/lib/values/project.ts
@@ -6,7 +6,7 @@ export const projects = [
       description:
         "A full-stack e-commerce web application built with Next.js, designed as the final project for IM-101. The platform includes user authentication, custom payment, customer and an admin dashboard.",
       image: "/abante-clothing.png",
-      technologies: ["Next.js", "TypeScript", "Tanstack Query", "Zustand", "Bun", "Prisma", "MySQL", "Tailwind CSS", "Shadcn UI", "Custom Payment", "Cloudinary", "Aiven", "Brevo API"],
+      technologies: ["Next.js", "TypeScript", "Tanstack Query", "Zustand", "Bun", "Prisma", "MySQL", "Tailwind CSS", "Shadcn UI", "Custom Payment", "Upstash Redis" ,"Cloudinary", "Aiven", "Brevo API"],
       liveUrl: "https://abante-clothing.vercel.app",
       githubUrl: "https://github.com/RANDAAAALL/Abante-Clothing",
       featured: true,
