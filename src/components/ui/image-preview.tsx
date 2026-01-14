071ba01cd850541addcd206e488bfa52ecc34e03
RANDAAAALL <lesterandig17@gmail.com>
2026-01-14 14:40:24 +0800

    refactor(image-preview): made some changes on styling

--- src/components/ui/image-preview.tsx
+++ src/components/ui/image-preview.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 import React from "react";
 import Image from "next/image";
 
@@ -12,34 +12,35 @@ export default function ImagePreviewContent() {
   if (!mounted) return null;
 
   return (
-    <div className="relative w-55 h-55 ml-5 group">
-
-      {/* default image (light theme, hides on hover or dark mode) */}
-      <Image
-        className={`
-          rounded-full object-cover
-          ${theme === "dark" ? "hidden" : "block"} 
-          group-hover:hidden
+    <div className="">
+      <div className="relative w-59 h-59 group">
+        {/* default image (light theme, hides on hover or dark mode) */}
+        <Image
+          className={`
+        rounded-sm object-cover
+        ${theme === "dark" ? "hidden" : "block"} 
+        group-hover:hidden
         `}
-        src="/randall-qt.png"
-        alt="profile-pic"
-        fill
-        sizes="360px"
-      />
+          src="/randall-qt.png"
+          alt="profile-pic"
+          fill
+          sizes="360px"
+        />
 
-      {/* hover image (dark theme: always visible or light theme: visible on hover) */}
-      <Image
-        className={`
-          rounded-full object-cover
-          ${theme === "dark" ? "block" : "hidden"}
+        {/* hover image (dark theme: always visible or light theme: visible on hover) */}
+        <Image
+          className={`
+        rounded-sm object-cover
+        ${theme === "dark" ? "block" : "hidden"}
           group-hover:block
         `}
-        src="/randall-qt-rayban.png"
-        alt="profile-pic-hover"
-        fill
-        sizes="360px"
-        priority
-      />
+          src="/randall-qt-rayban.png"
+          alt="profile-pic-hover"
+          fill
+          sizes="360px"
+          priority
+        />
+      </div>
     </div>
   );
 }
